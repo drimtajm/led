2b514ff1db38103e317e80d425cfe3579c5b1973
drimtajm <angela.johansson@gmail.com>
2015-12-09 17:34:27 +0100

    Add support for colours and make web page for double matrix
    Note: Common code should be in a library/javascript file instead
    Functions could be appended with type, e.g. to_picture_led_array

--- erlang/led_array.erl
+++ erlang/led_array.erl
@@ -4,13 +4,6 @@
 
 -define(CE_PIN, 8).
 
--define(CHAR0, [16#3C,16#42,16#42,16#42,16#42,16#42,16#42,16#3C]).
--define(CHAR1, [16#10,16#30,16#50,16#10,16#10,16#10,16#10,16#7C]).
--define(SPIRAL, [2#11111111, 2#00000001, 2#11111101, 2#10000101,
-		 2#10100101, 2#10111101, 2#10000001, 2#11111111]).
--define(TEST_LIST, ["A1", "B2", "C3", "D4",
-		    "A8", "B7", "C6", "D5", "E4", "F3", "G2", "H1"]).
-
 spi_write(Handle, Register, Value) ->
     spi_interface:transfer_spi_data(Handle, [Register, Value]).
 
@@ -30,8 +23,9 @@ init(Handle) ->
 
 display_picture(_Handle, [], _N) ->
     ok;
-display_picture(Handle, [Value | Rest], N) ->
-    spi_write(Handle, (16#28 + (N - 1)), Value),
+display_picture(Handle, [{ValueRed, ValueGreen} | Rest], N) ->
+    spi_write(Handle, (16#20 + (N - 1)), ValueRed),
+    spi_write(Handle, (16#28 + (N - 1)), ValueGreen),
     display_picture(Handle, Rest, N+1).
 
 display_picture(Handle, Picture) ->
@@ -48,10 +42,13 @@ to_picture(List) ->
 to_picture(_List, Result, 9) ->
     lists:reverse(Result);
 to_picture(List, Result, N) ->
-    SubList = [X-$A+1 || [X, N0] <- List,
-			 N == N0 - $0],
-    RowValue = convert_indeces_to_int(SubList),
-    to_picture(List, [RowValue | Result], N+1).
+    SubListRed = [X-$A+1 || {[X, N0], C} <- List,
+			    N == N0 - $0, C rem 2 == 1],
+    SubListGreen = [X-$A+1 || {[X, N0], C} <- List,
+			    N == N0 - $0, C >= 2],
+    RowValueRed = convert_indeces_to_int(SubListRed),
+    RowValueGreen = convert_indeces_to_int(SubListGreen),
+    to_picture(List, [{RowValueRed, RowValueGreen} | Result], N+1).
 
 line([X1, Y], [X2, Y]) ->
     lists:map(fun (X) ->
@@ -74,9 +71,6 @@ handle_code(Code) ->
     erl_eval:exprs(ErlAbsForm,Bindings),
     ok.
 
-display([{_Coordinate, _Colour} | _] = List) ->
-    display([Coordinate || {Coordinate, Colour} <- List,
-			   Colour == 1]);
 display(List) ->
     Picture = to_picture(List),
     Handle = get(handle),
@@ -105,16 +99,9 @@ go() ->
     {ok, Handle} = spi_interface:open_spi_bus(?CE_PIN),
     init(Handle),
     put(handle, Handle),
-    Zeros = lists:duplicate(8, 0),
+    Zeros = lists:duplicate(8, {0, 0}),
     display_picture(Handle, Zeros),
     receive_loop(),
-   %%display_picture(Handle, ?CHAR0),
-   %%timer:sleep(200),
-   %%display_picture(Handle, ?CHAR1),
-   %%timer:sleep(1000),
-   %%display_picture(Handle, ?SPIRAL),
-   %%timer:sleep(500),
-    Zeros = lists:duplicate(8, 0),
     display_picture(Handle, Zeros),
     %%Configuration = spi_read(Handle, 4),
     %%spi_write(Handle, 4, Configuration band 2#11111110),
