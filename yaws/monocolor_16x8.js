fb1409f2c92b7bb1802e4657003bba339a925f3e
Mathias Bergvall <mathias.bergvall@gmail.com>
2015-12-09 23:02:55 +0100

    Added clear button Inital support for setting multiple leds by click and move Extract and generalize common code to handle both multicolor and single color matrices with different dimensions

---
+++ yaws/monocolor_16x8.js
@@ -0,0 +1,4 @@
+var COLORS = [ '#f0f0f0', '#FF6B70'];
+var COLUMNS = 16;
+var ROWS = 8;
+var MULTICOLOR = false;
---
+++ yaws/multicolor_8x8.js
@@ -0,0 +1,4 @@
+var COLORS = [ '#f0f0f0', '#FF6B70', '#6BFF97', '#FFBB2B' ];
+var COLUMNS = 8;
+var ROWS = 8;
+var MULTICOLOR = true;
